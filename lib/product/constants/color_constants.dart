e2346d403eb2e3bde5bfedf7f005ba74d8ff0125
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-03-26 02:51:54 +0300

    create - constants

---
+++ lib/product/constants/color_constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const Color buttonColor = Color(0xFFC67C4E);
+  static const Color whiteColor = Colors.white;
+  static const Color blackColor = Colors.black;
+  static const Color greyColor = Color(0xFF313131);
+  static const Color lightGreyColor = Color(0xFFA2A2A2);
+}
---
+++ lib/product/constants/string_constants.dart
@@ -0,0 +1,9 @@
+class StringConstants {
+  static const String getStarted = 'Get Started';
+  static const String onboardingTitle =
+      'Fall in Love with\nCoffee in Blissful\nDelight!';
+  static const String onboardingDescription =
+      'Welcome to our cozy coffee corner, where\nevery cup is a delightful for you.';
+  static const String location = 'Location';
+  static const String newYork = 'New York, USA';
+}
