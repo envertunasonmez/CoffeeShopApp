04208d31aba4dc43519ff6debfc832b3227eb1a1
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-03-27 04:21:13 +0300

    add - coffee category list to home view

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_shop_app/view/home/widgets/coffee_category_list.dart';
 import 'package:coffee_shop_app/view/home/widgets/location_header.dart';
 import 'package:coffee_shop_app/view/home/widgets/promo_card.dart';
 import 'package:flutter/material.dart';
@@ -22,12 +23,17 @@ class HomeView extends StatelessWidget {
             right: 18,
             child: PromoCard(size: size),
           ),
+          Positioned(
+            top: size.height * 0.52,
+            left: 0,
+            right: 0,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: CoffeeCategoryList(),
+            ),
+          ),
         ],
       ),
     );
   }
 }
-
-
-
-
