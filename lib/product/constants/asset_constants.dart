b0fd92ab6b7dfefb65d543e157c882d51d59519d
Enver Tuna Sönmez <etsonmez01@gmail.com>
2025-03-27 03:44:38 +0300

    add - image and icons

--- lib/product/constants/asset_constants.dart
+++ lib/product/constants/asset_constants.dart
@@ -1,7 +1,10 @@
 class AssetConstants {
   // images
   static const onboardingPic = 'assets/images/onboarding_pic.png';
+  static const banner = 'assets/images/banner.png';
 
   // icons
   static const iconDropDown = 'assets/icons/ic_dropdown.png';
+  static const iconFilter = 'assets/icons/ic_filter.png';
+  static const iconSearch = 'assets/icons/ic_search.png';
 }
